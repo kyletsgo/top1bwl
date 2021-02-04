98beb292d975596a5883e6b3c7d95ac30a59efda
kyle <kylecw.lin@ogilvy.com>
2021-02-04 18:11:36 +0800

    需求修改

--- public/assets/template/src/js/layout/swiper.js
+++ public/assets/template/src/js/layout/swiper.js
@@ -1,16 +1,26 @@
 var swiper = new Swiper('.carousel__normal', {
     slidesPerView: 1,
     spaceBetween: 0,
-    effect: 'fade',
+    autoHeight: true,
     loop: true,
     autoplay: {
-        delay: 2500,
+        delay: 5000,
         disableOnInteraction: false,
     },
     navigation: {
         nextEl: '.swiper-button-next1',
         prevEl: '.swiper-button-prev1',
     },
+    breakpoints: {
+        480: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+      }
 });
 
 var swiper1 = new Swiper('.sale__bar', {
