31173e203814e948ea69325fb8e7383c2542181f
kyle <kylecw.lin@ogilvy.com>
2021-02-06 18:19:15 +0800

    行事曆區

--- app/Http/Controllers/Backend/CalendarController.php
+++ app/Http/Controllers/Backend/CalendarController.php
@@ -41,6 +41,9 @@ class CalendarController extends Controller
      */
     public function edit(Request $request)
     {
+        error_reporting(E_ALL);
+        ini_set('display_errors', 1);
+        
         $image_large = '';
         if ($request->hasFile('largeImage')) {
             $orig_image_name = 'calendar-largeImage-' . time() . '.jpg';
